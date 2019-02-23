91eea34892b701d35f846e3f969b227833f9658f
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 02:47:08 +0500

    Configure routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router'
+import { ConnectedRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
 
 import configureStore from './store'
 import AppContainer from './containers/App'
 
+const history = createBrowserHistory()
+
+const store = configureStore(history)
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <AppContainer/>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path='/' component={AppContainer}/>
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
 )
--- src/store.js
+++ src/store.js
@@ -3,8 +3,8 @@ import { createStore } from 'redux'
 import rootReducer from './reducers'
 import middleware from './middleware'
 
-export default (initialState = {}) => createStore(
-  rootReducer,
+export default (history, initialState = {}) => createStore(
+  rootReducer(history),
   initialState,
   middleware,
 )
