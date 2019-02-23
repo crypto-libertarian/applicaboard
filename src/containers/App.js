543b2a3ee0724e523682099966637e13684910a0
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 02:25:07 +0500

    Move containers to separate directory

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-class App extends Component {
+class AppContainer extends Component {
   render() {
     return <p>Hello, World!</p>
   }
 }
 
-export default connect()(App)
+export default connect()(AppContainer)
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import configureStore from './store'
-import App from './App'
+import AppContainer from './containers/App'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App/>
+    <AppContainer/>
   </Provider>,
   document.getElementById('root'),
 )
