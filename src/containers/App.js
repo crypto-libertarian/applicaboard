6c1f92d2706d50a4e4c9b23ac5ad5e114726f666
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 02:26:46 +0500

    Map state to props

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+const mapStateToProps = state => ({...state})
+
 class AppContainer extends Component {
   render() {
     return <p>Hello, World!</p>
   }
 }
 
-export default connect()(AppContainer)
+export default connect(mapStateToProps)(AppContainer)
