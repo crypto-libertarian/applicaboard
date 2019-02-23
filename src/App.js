b9fe2b36287f7113735cb60deebb9d3f95915677
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 02:24:07 +0500

    Connect container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return <p>Hello, World!</p>
   }
 }
+
+export default connect()(App)
