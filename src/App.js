32666b48d2dbe920bb12e208dba1877d8b6fbe14
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 01:21:28 +0500

    Fix code style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -21,8 +21,6 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
-
-export default App;
