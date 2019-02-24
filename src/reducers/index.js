df0498b5ef7a6531cc739c1d696ecef2a867da37
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 07:24:07 +0500

    Configure Drizzle reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
+import { drizzleReducers } from 'drizzle'
 
 export default history => combineReducers({
   router: connectRouter(history),
+  ...drizzleReducers,
 })
