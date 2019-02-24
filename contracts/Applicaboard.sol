bf4432b457f9c6a871d288e3af53b21d558a1df8
Alex Kotov <kotovalexarian@gmail.com>
2019-02-25 02:13:57 +0500

    Add contract function Applicaboard.createResponse

--- contracts/Applicaboard.sol
+++ contracts/Applicaboard.sol
@@ -1,20 +1,45 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Applicaboard {
-  event NewApplication(uint applicationId, address applicant, string text);
+  event NewApplication(uint applicationId,
+                       address applicant,
+                       string text);
+
+  event NewResponse(uint applicationId,
+                    address applicant,
+                    string text,
+                    string response);
 
   struct Application {
     address applicant;
     string text;
+    bool resolved;
+    string response;
   }
 
   Application[] public _applications;
 
   function createApplication(string memory text) public {
-    Application memory application = Application(msg.sender, text);
+    Application memory application = Application(msg.sender, text, false, '');
+
     uint applicationId = _applications.push(application) - 1;
+
     emit NewApplication(applicationId,
                         application.applicant,
                         application.text);
+  }
+
+  function createResponse(uint applicationId, string memory response) public {
+    Application storage application = _applications[applicationId];
+
+    require(!application.resolved);
+
+    application.resolved = true;
+    application.response = response;
+
+    emit NewResponse(applicationId,
+                     application.applicant,
+                     application.text,
+                     application.response);
   }
 }
