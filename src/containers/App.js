edaf97c36733e9fba3594486ede1088b14583c5e
Alex Kotov <kotovalexarian@gmail.com>
2019-02-24 08:01:38 +0500

    Configure Drizzle context consumer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { DrizzleContext } from 'drizzle-react'
 
 const mapStateToProps = state => ({...state})
 
 class AppContainer extends Component {
   render() {
+    return (
+      <DrizzleContext.Consumer>
+        {drizzleContext => {
+          const { drizzle, drizzleState, initialized } = drizzleContext
+          return this.renderContent(drizzle, drizzleState, initialized)
+        }}
+      </DrizzleContext.Consumer>
+    )
+  }
+
+  renderContent(drizzle, drizzleState, initialized) {
+    if (!initialized) {
+      return <p>Loading...</p>
+    }
+
     return <p>Hello, World!</p>
   }
 }
