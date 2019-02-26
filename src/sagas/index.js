e76f83eaac3439acab6cdf3c90595d8af65a030d
Alex Kotov <kotovalexarian@gmail.com>
2019-02-26 10:48:06 +0500

    Enable Drizzle sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,2 +1,9 @@
+import { all, fork } from 'redux-saga/effects'
+
+import { drizzleSagas } from 'drizzle'
+
 export default function*() {
+  yield all(
+    drizzleSagas.map(saga => fork(saga)),
+  )
 }
