aec9cd90411e4e80216c4c116ae56a7f486a601e
Alex Kotov <kotovalexarian@gmail.com>
2019-02-28 11:32:27 +0500

    Add ApplicationsListComponent

---
+++ src/components/ApplicationsList.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import Table from 'react-bootstrap/Table'
+
+import ApplicationComponent from '../components/Application'
+
+export default class ApplicationsListComponent extends React.Component {
+  state = { dataKey: null }
+
+  componentDidMount() {
+    const { drizzle } = this.props
+    const contract = drizzle.contracts.Applicaboard
+    const dataKey = contract.methods['getApplicationsCount'].cacheCall()
+    this.setState({ dataKey })
+  }
+
+  render() {
+    const { drizzle, drizzleState } = this.props
+    const contract = drizzleState.contracts.Applicaboard
+    const data = contract.getApplicationsCount[this.state.dataKey]
+
+    let value = 0
+
+    if (data && data.value) {
+      value = parseInt(data.value)
+    }
+
+    if (value < 0 || isNaN(value) || !isFinite(value)) {
+      value = 0
+    }
+
+    return (
+      <div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Applicant</th>
+              <th>Text</th>
+              <th>Response</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...Array(value)].map((e, i) =>
+              <ApplicationComponent
+                key={i}
+                drizzle={drizzle}
+                drizzleState={drizzleState}
+                applicationId={i}
+              />
+            )}
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { DrizzleContext } from 'drizzle-react'
 
-import Table from 'react-bootstrap/Table'
-
-import ApplicationComponent from '../components/Application'
+import ApplicationsListComponent from '../components/ApplicationsList'
 
 const mapStateToProps = state => ({...state})
 
@@ -24,62 +22,8 @@ class AppContainer extends React.Component {
       return <p>Loading...</p>
     }
 
-    return <Foo drizzle={drizzle} drizzleState={drizzleState}/>
-  }
-}
-
-class Foo extends React.Component {
-  state = { dataKey: null }
-
-  componentDidMount() {
-    const { drizzle } = this.props
-
-    const contract = drizzle.contracts.Applicaboard
-
-    const dataKey = contract.methods['getApplicationsCount'].cacheCall()
-
-    this.setState({ dataKey })
-  }
-
-  render() {
-    const { drizzle, drizzleState } = this.props
-
-    const contract = drizzleState.contracts.Applicaboard
-
-    const data = contract.getApplicationsCount[this.state.dataKey]
-
-    let value = 0
-
-    if (data && data.value) {
-      value = parseInt(data.value)
-    }
-
-    if (isNaN(value)) {
-      value = 0
-    }
-
     return (
-      <div>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Applicant</th>
-              <th>Text</th>
-              <th>Response</th>
-            </tr>
-          </thead>
-          <tbody>
-            {[...Array(value)].map((e, i) =>
-              <ApplicationComponent
-                key={i}
-                drizzle={drizzle}
-                drizzleState={drizzleState}
-                applicationId={i}
-              />
-            )}
-          </tbody>
-        </Table>
-      </div>
+      <ApplicationsListComponent drizzle={drizzle} drizzleState={drizzleState}/>
     )
   }
 }
