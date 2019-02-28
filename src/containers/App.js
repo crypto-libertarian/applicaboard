559970c0fd1a521f7deb92af0860237483e1175b
Alex Kotov <kotovalexarian@gmail.com>
2019-02-28 12:57:59 +0500

    Improve layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { DrizzleContext } from 'drizzle-react'
 
+import Container from 'react-bootstrap/Container'
+import Row       from 'react-bootstrap/Row'
+import Col       from 'react-bootstrap/Col'
+
 import ApplicationsListComponent from '../components/ApplicationsList'
 
 class AppContainer extends React.Component {
@@ -21,7 +25,16 @@ class AppContainer extends React.Component {
     }
 
     return (
-      <ApplicationsListComponent drizzle={drizzle} drizzleState={drizzleState}/>
+      <Container>
+        <Row>
+          <Col>
+            <ApplicationsListComponent
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+            />
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
